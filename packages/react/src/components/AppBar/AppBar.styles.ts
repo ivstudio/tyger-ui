702a8356295178ca17d682ecb004f2849837ff96
Iggy Villamar <iggy@ivstudio.com>
2023-01-22 12:57:28 -0500

    Add AppBar z-index

--- packages/react/src/components/AppBar/AppBar.styles.ts
+++ packages/react/src/components/AppBar/AppBar.styles.ts
@@ -14,6 +14,7 @@ export const AppBarBase = styled.header<{
     align-items: center;
     box-sizing: border-box;
     backdrop-filter: blur(20px);
+    z-index: ${({ theme: { zIndex } }) => zIndex.appBar};
     background-color: ${({ theme: { appBar } }) => appBar.backgroundColor};
     color: ${({ theme: { appBar } }) => appBar.color};
     padding: ${({ theme: { spacing } }) => `0 ${spacing['16']}`};
