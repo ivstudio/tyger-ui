e8a08752ebd060b17cec947f7dc8013b85cd4a96
Iggy Villamar <iggy@ivstudio.com>
2023-03-11 11:46:17 -0500

    Update rollup plugin-terser

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 
 const createStyledComponentsTransformer =
