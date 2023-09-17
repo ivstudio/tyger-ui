c294136d8ff78000037b2ab2f01a893d649a982a
Iggy Villamar <iggy@ivstudio.com>
2023-09-17 14:32:14 -0400

    Add unit Input component test

--- packages/react/src/components/Input/input.test.tsx
+++ packages/react/src/components/Input/input.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '../../../test/utils/mockProvider';
+import { fireEvent, render } from '../../../test/utils/mockProvider';
 import Input from './Input';
 
 describe('Input', () => {
@@ -13,5 +13,28 @@ describe('Input', () => {
         const { getByRole } = render(<Input disabled />);
         const inputElement = getByRole('textbox');
         expect(inputElement).toHaveAttribute('disabled');
+    });
+
+    it('displays error message when error prop is true', () => {
+        const { getByText } = render(
+            <Input error helperText="Error message" />
+        );
+        const errorMessageElement = getByText('Error message');
+        expect(errorMessageElement).toBeInTheDocument();
+    });
+
+    it('does not display error message when error prop is false', () => {
+        const { queryByText } = render(<Input helperText="Helper text" />);
+        const errorMessageElement = queryByText('Error message');
+        expect(errorMessageElement).toBeNull();
+    });
+
+    it('calls onChange callback when input value changes', () => {
+        const onChangeMock = jest.fn();
+        const { getByRole } = render(<Input onChange={onChangeMock} />);
+        const inputElement = getByRole('textbox') as HTMLInputElement;
+        fireEvent.change(inputElement, { target: { value: 'John Doe' } });
+        expect(onChangeMock).toHaveBeenCalledTimes(1);
+        expect(inputElement.value).toBe('John Doe');
     });
 });
