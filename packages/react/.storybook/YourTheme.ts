f8c2f6c4e4b5f23f097ddeda5ce0776ca418c2bc
Iggy Villamar <iggy@ivstudio.com>
2023-09-18 06:53:18 -0400

    Update SB theme with logo

---
+++ packages/react/.storybook/YourTheme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'dark',
+    brandTitle: 'Tyger UI',
+    brandImage:
+        'https://tyger.s3.us-west-2.amazonaws.com/assets/tyger-logo-light.svg',
+    brandTarget: '_self',
+});
